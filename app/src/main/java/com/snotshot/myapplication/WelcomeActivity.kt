8dc94e5fc24c51b232c934dc648aba632be134fc
peacemaker4 <esmagametov.bek@gmail.com>
2022-04-14 12:30:48 +0600

    welcome page update

--- app/src/main/java/com/snotshot/myapplication/WelcomeActivity.kt
+++ app/src/main/java/com/snotshot/myapplication/WelcomeActivity.kt
@@ -3,6 +3,7 @@ package com.snotshot.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.snotshot.myapplication.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
@@ -10,12 +11,16 @@ class WelcomeActivity : AppCompatActivity() {
     //View binding
     private lateinit var binding: ActivityWelcomeBinding
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        //check for user authorization
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
 
         // Register
         binding.signUpBtn.setOnClickListener{
@@ -25,6 +30,15 @@ class WelcomeActivity : AppCompatActivity() {
         // Login
         binding.signInBtn.setOnClickListener{
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    private fun checkUser() {
+        //if user is already logged in go to main activity
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
